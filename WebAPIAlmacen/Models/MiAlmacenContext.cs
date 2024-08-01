0c1b7b3bf9308d844753d8c6013d1fad0c8a2a8e
Juan Luis Ochoa <jlprofesor8@gmail.com>
2024-08-01 17:55:15 +0200

    Scaffold

---
+++ WebAPIAlmacen/Models/MiAlmacenContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPIAlmacen.Models;
+
+public partial class MiAlmacenContext : DbContext
+{
+    public MiAlmacenContext()
+    {
+    }
+
+    public MiAlmacenContext(DbContextOptions<MiAlmacenContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Familia> Familias { get; set; }
+
+    public virtual DbSet<Producto> Productos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=MiAlmacen;Integrated Security=True;TrustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Familia>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Familias__3214EC07F3B989A2");
+
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<Producto>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Producto__3214EC071B987D5B");
+
+            entity.Property(e => e.FotoUrl).HasColumnName("FotoURL");
+            entity.Property(e => e.Nombre).HasMaxLength(150);
+            entity.Property(e => e.Precio).HasColumnType("decimal(9, 2)");
+
+            entity.HasOne(d => d.Familia).WithMany(p => p.Productos)
+                .HasForeignKey(d => d.FamiliaId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Familias_Productos");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
