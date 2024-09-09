f65cf9b40d586606af3755659c3cff3de941b8bc
Juan Luis Ochoa <jlprofesor8@gmail.com>
2024-09-09 14:59:37 +0200

    Actualizados app.componet.ts y app.component.html

--- WebAPIAlmacen/AngularAlmacen/src/app/app.component.ts
+++ WebAPIAlmacen/AngularAlmacen/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SignalrService } from './services/signalr.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularAlmacen';
+  get connected() {
+    return this.signalRService.connected;
+  }
+
+  constructor(private signalRService: SignalrService) {}
 }
